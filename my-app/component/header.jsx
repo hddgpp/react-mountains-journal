482b0124c495b8abd45843d38453a5dd90b6455f
hddgpp <youssefhehe3@gmail.com>
2025-09-10 13:55:31 +0100

    a new projct

---
+++ my-app/component/header.jsx
@@ -0,0 +1,10 @@
+export function Header() {
+    return (
+    <header>
+        <nav className="nav-header ">
+            <img src="/Daco_4629036.png" alt="logo" className="logo" />
+            <h1>Travel journal</h1>
+        </nav>
+    </header>
+    )
+}
---
+++ my-app/src/App.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../component/header";
+import { Entery } from "../component/Entery.jsx";
+
+export function App() {
+  return(
+    <>
+      <Header/>
+      <Entery/>
+    </>
+  )
+}
